ffbec3a736981fa8f3107697f6df2b24e7261983
ggamarin <gam110191@gmail.com>
2020-05-05 18:34:39 +0300

    initial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostToPostman() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("postman.schema.json"))
+        ;
+    }
+}
