ffb0670e293e44ff73c6c6c33a357596492d0abe
ggamarin <gam110191@gmail.com>
2020-05-05 18:43:59 +0300

    correct test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -11,7 +11,7 @@ public class PostmanEchoTest {
 
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some dat")
+                .body("some data")
                 .when()
                 .post("/post")
                 .then()
